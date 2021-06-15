e9a82a70e0a8825f968a6a0ed511d8395b16d09a
mihai <mihai.rachitan@gmail.com>
2021-06-15 10:03:21 +0300

    De testat

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from question_model import Question
-from data import question_data
+# from data import question_data
+# de schimbat in question_bank.append(Question(elem--->>["question"], elem--->>["correct_answer"].lower()))
 from quiz_brain import QuizBrain
+from data1 import question_data
 
 question_bank = []
 
 for elem in question_data:
-    question_bank.append(Question(elem["text"], elem["answer"].lower()))
+    question_bank.append(Question(elem["question"], elem["correct_answer"].lower()))
 
 quiz = QuizBrain(question_bank)
 
